0f3f5f9cf9a1d184ef0393577cd4d01e174986e7
v.kulasekar <v.kulasekar@CDC2-D-441SR3Y.dir.svc.accenture.com>
2018-02-23 11:53:00 +0530

    Query update

--- src/main/resources/queries.sql
+++ src/main/resources/queries.sql
@@ -9,3 +9,5 @@ create table csvfiles(id int(10) AUTO_INCREMENT, filename varchar(50), fileconte
 -- ArffFile
 create table arfffiles(id int(10) AUTO_INCREMENT, filename varchar(50), filecontent BLOB, excelId int(10),csvid int(10), primary key(id), FOREIGN KEY (excelId) REFERENCES excelfiles(id),FOREIGN KEY (csvid) REFERENCES csvfiles(id));
 
+---adding rowcount column in excelfiles table
+alter table excelfiles add column rowcount int(10);
