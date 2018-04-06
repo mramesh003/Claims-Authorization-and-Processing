3407fd1522763a6e501f6aee485d153bdbc753d0
menaka.a.ganesan <menaka.a.ganesan@CDC2-D-511QFYT.dir.svc.accenture.com>
2018-04-06 17:13:40 +0530

    Code changes for Python code

--- Python/com/accenture/MLengine/claim_process_engine.py
+++ Python/com/accenture/MLengine/claim_process_engine.py
@@ -1,8 +1,6 @@
 import pandas
 import pickle
-import itertools
 import warnings
-import numpy as np
 import matplotlib.pyplot as plt
 from sklearn import preprocessing
 from sklearn import model_selection
@@ -13,42 +11,9 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 from sklearn.naive_bayes import GaussianNB
-import mysql.connector
 from io import BytesIO
-
-
-class plot:
-
-    def plot_confusion_matrix(matrix, classes,
-                              normalize=False,
-                              title='Confusion matrix',
-                              confusionMatrix=plt.cm.Blues):
-
-        plt.imshow(matrix, interpolation='nearest', cmap=confusionMatrix)
-        plt.title(title)
-        plt.colorbar()
-        tick_marks = np.arange(len(classes))
-        plt.xticks(tick_marks, classes, rotation=45)
-        plt.yticks(tick_marks, classes)
-
-        plot.plot_box_iteration(matrix, normalize=False)
-
-        plt.tight_layout()
-        plt.ylabel('True label')
-        plt.xlabel('Predicted label')
-
-        plt.show()
-
-    def plot_box_iteration(matrix, normalize=False):
-
-        fmt = '.2f' if normalize else 'd'
-        thresh = matrix.max() / 2.
-        for i, j in itertools.product(range(matrix.shape[0]), range(matrix.shape[1])):
-            plt.text(j, i, format(matrix[i, j], fmt),
-                     horizontalalignment="center",
-                     color="white" if matrix[i, j] > thresh else "black")
-
-
+import com.accenture.MLengine.database.db_connection as db
+import com.accenture.MLengine.graph.matrix as Plot
 
 class claim_adjudication:
 
@@ -64,8 +29,8 @@ class claim_adjudication:
 
         # Split-out validation dataset
         array = dataset.values
-        X = array[:,0:25]
-        Y = array[:,25]
+        X_Dataset= array[:,0:25]
+        Y_Dataset = array[:,25]
 
         str_val = [2,4,6,8,11,12,13,18,19,20,21,23]
 
@@ -73,22 +38,19 @@ class claim_adjudication:
 
         for val in str_val:
             enc = preprocessing.LabelEncoder()
-            enc.fit(X[:,val])
-            X[:,val] = enc.transform(X[:,val])
+            enc.fit(X_Dataset[:,val])
+            X_Dataset[:,val] = enc.transform(X_Dataset[:,val])
 
-        return X,Y;
+        return X_Dataset,Y_Dataset;
+
 
 
     def multi_algo_result(self,X_train, Y_train):
-
-        # Spot Check Algorithms
         models = []
         models.append(('Linear Discriminant', LinearDiscriminantAnalysis()))
         models.append(('KNeighbors', KNeighborsClassifier()))
         models.append(('Decision Tree', DecisionTreeClassifier()))
         models.append(('GaussianNB', GaussianNB()))
-
-        # evaluate each model in turn
         results = []
         names = []
         for name, model in models:
@@ -98,33 +60,22 @@ class claim_adjudication:
             names.append(name)
             msg = "%s: %f (%f)" % (name, cv_results.mean(), cv_results.std())
             print(msg)
-
         return results, names;
 
     def fit_and_train_Model(self,X_trainset, Y_trainset):
-        # Make predictions on validation dataset
         dtc = DecisionTreeClassifier()
         dtc.fit(X_trainset, Y_trainset)
         return dtc;
 
- #Connection Function
-    def dbconnection(self):
-        cnx = mysql.connector.connect(user='user1', password='user1',
-                                      host='10.220.103.153',
-                                      database='claimsauthprocess')
-        if cnx.is_connected():
-            return cnx;
-
     def savemodel(self,dtc,csv_list):
-
         modelcontent = pickle.dumps(dtc)
-        connection =self.dbconnection()
+        connection =db_conn.dbconnection()
         cursor = connection.cursor()
-        cursor.execute("insert into modelfiles(filename,filecontent,datacount,colcount,flag) values(%s,%s,%s,%s,%s)",(csv_list[0],modelcontent,csv_list[1],csv_list[2],"python"))
+        cursor.execute("insert into modelfiles(filename,filecontent,datacount,colcount,flag) values(%s,%s,%s,%s,%s)",(csv_list[0],modelcontent,csv_list[2],csv_list[1],"python"))
         connection.commit()
 
     def loadModel(self):
-            connection = self.dbconnection()
+            connection = db_conn.dbconnection()
             cursor = connection.cursor()
             query1 = ("SELECT filecontent from modelfiles where flag=%s order by id DESC limit 1")
             cursor.execute(query1,('python',))
@@ -133,35 +84,27 @@ class claim_adjudication:
             return model;
 
     def executeModel(self,result, X_validationset,  Y_validationset):
-
         predictions = result.predict(X_validationset)
         print("                           ")
         print("===========================")
         print("Model Prediction End Result")
         print("===========================")
-
         acc_score = accuracy_score(Y_validationset, predictions)
         print(acc_score)
         print("                           ")
         print("===========================")
         print("Model Confusion Matrix")
         print("===========================")
-
         cnf_matrix = confusion_matrix(Y_validationset, predictions)
-
-        print(cnf_matrix)
-        class_names = ['Accept', 'Pend', 'Reject']
-        plot.plot_confusion_matrix(cnf_matrix, classes=class_names,
-                              title='Confusion Matrix')
+        matrix_obj.plot_confusion_matrix(cnf_matrix)
         print("===========================")
         print("Model Classification Report")
         print("===========================")
         print(classification_report(Y_validationset, predictions))
-        return predictions;
+        return predictions,cnf_matrix;
 
 
     def generatePlot(self,results, names):
-        # Compare Algorithms
         fig = plt.figure()
         fig.suptitle('Algorithm Comparison')
         ax = fig.add_subplot(111)
@@ -171,30 +114,20 @@ class claim_adjudication:
         return;
 
     def csvretrival(self,fileid):
-     #connection established
-        connection = self.dbconnection()
+        connection =db_conn.dbconnection()
         cursor = connection.cursor()
         query1 = ("SELECT * from csvfiles where id = %s")
         cursor.execute(query1,(fileid,))
         data = cursor.fetchone()
-
         filename = str(data[1])
-    # for saving the filename
         modelfile = filename.strip(".csv")
         modelname = modelfile + ".mdl"
-    # row Count
         columncount = data[4]
-    # column count
         datacount = data[5]
         csv_filecontent = str(data[2], 'ISO-8859-1')
         csv_list = [modelname,datacount,columncount]
         csv_bytecontent = bytes(csv_filecontent, 'utf-8')
-    #Creating the buffer
         csv_buffercontent = BytesIO(csv_bytecontent)
-     # header
-     # dynamic header reading -------------------------------------------------------------
-     # my_string = csv_filecontent.splitlines()[0]
-     # names = my_string.split(',')
 
         names = ['Billing National Provider Identification Number',
              'Insured Policy Number for Subscriber',
@@ -204,7 +137,7 @@ class claim_adjudication:
              'Payer Identification',
              'Patient State',
              'Patient Zip Code',
-             'Patient’s Birth Date',
+             'Patientâ€™s Birth Date',
              'Facility Type Code',
              'Claim Transaction Type',
              'Statement From and Statement Through Date',
@@ -222,55 +155,34 @@ class claim_adjudication:
              'Service Date',
              'Service Facility Provider ID',
              'Claim Status']
-     #Returning the file content as buffer and header
-        return csv_buffercontent, names,csv_list;
+        return csv_buffercontent,names,csv_list;
 
+    def Loadimage(self):
+        with open("Results.png", "rb")as imageFile:
+            image = imageFile.read()
+            Image_Bytes = bytearray(image)
+            return Image_Bytes;
 
     def train_model(self,fileid):
-    #retriving the csv file from DB
-        csv_buffercontent , names1,csv_list =self.csvretrival(fileid)
-    #forming the Dataset
+        csv_buffercontent,names1,csv_list =self.csvretrival(fileid)
         X, Y = self.form_dataset(csv_buffercontent,names1)
-    # Call fit_and_train_Model(X, Y) function# Fit and Save the model output
         dtc = self.fit_and_train_Model(X, Y)
-    # Save the model file in DB
         self.savemodel(dtc,csv_list)
-    #Returning the Model
         return 'success';
 
     def execute_model(self,testfileId):
-        # Need to load the test CSV file from DB and form the Dataset
         csv_buffercontent, names1,csv_list = self.csvretrival(testfileId)
         X, Y = self.form_dataset(csv_buffercontent, names1)
-        # Need to load the latest model file from DB
         result = self.loadModel()
-
-        #splitting and generate plot ----------
-        #X_train, X_validation, Y_train, Y_validation = model_selection.train_test_split(X, Y, test_size=validation_size,random_state=5)
-        #results, names = self.multi_algo_result(X_train, Y_train)
-        #self.generatePlot(results, names)
-
-        # call executeModel(dtc, X_validationset,  Y_validationset) function
-        predictions =self.executeModel(result, X, Y)
-
-        #append logic ----------------
-        # predictions = result.predict(X)
-        # print(predictions)
-        # result1 = []
-        # index = 0
-        # a = iter(X)
-        # for i in a:
-        #     j= np.append(i,predictions[index])
-        #     result1.append(j)
-        #     index = index + 1
-
-
-        ## returning predictions - example['Accept','Pend','Accept'...]
-        return predictions;
-#Assigning size for the test data file (10%)
+        predictions,cnfmatrix = self.executeModel(result, X, Y)
+        predictions_confmatrix = ','.join(str(e) for e in predictions) + "result" + ','.join(str(e) for e in cnfmatrix) +"result"
+        return predictions_confmatrix;
+#-----------------------------------------------------------------------------------
 validation_size = 0.10
 
-
+#Object creation
+db_conn = db.db_Connection()
+matrix_obj = Plot.plot()
 
 
 
---
+++ Python/com/accenture/MLengine/graph/matrix.py
@@ -0,0 +1,29 @@
+import itertools
+import numpy as np
+import matplotlib.pyplot as plt
+
+class plot:
+
+    def plot_confusion_matrix(self,matrix):
+        class_names = ['Accept', 'Pend', 'Reject']
+        confusion_matrix = plt.cm.Blues
+        plt.imshow(matrix, interpolation='nearest', cmap=confusion_matrix)
+        plt.title('Confusion matrix')
+        plt.colorbar()
+        tick_marks = np.arange(len(class_names))
+        plt.xticks(tick_marks, class_names, rotation=45)
+        plt.yticks(tick_marks, class_names)
+        plot.plot_box_iteration(matrix, False)
+        plt.tight_layout()
+        plt.ylabel('True label')
+        plt.xlabel('Predicted label')
+        plt.savefig('Results.png')
+        plt.show()
+
+    def plot_box_iteration(matrix, normalize=False):
+        fmt = '.2f' if normalize else 'd'
+        thresh = matrix.max() / 2.
+        for i, j in itertools.product(range(matrix.shape[0]), range(matrix.shape[1])):
+            plt.text(j, i, format(matrix[i, j], fmt),
+                     horizontalalignment="center",
+                     color="white" if matrix[i, j] > thresh else "black")
